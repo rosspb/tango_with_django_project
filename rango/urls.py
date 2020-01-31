975d5b8d7eccffd05db402526baf1986e164726e
Ross Beech <2141893b@student.gla.ac.uk>
2020-01-31 14:51:15 +0000

    updating models

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,8 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
+    #in views file the function is called index and that is why we call it below
+    #we leave the first argument blank as we are calling index of rango app
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
 ]
