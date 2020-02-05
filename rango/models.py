3b3e3abdd0cd2cc76e666b0cb72cfb5e6fbf3f15
Ross Beech <2141893b@student.gla.ac.uk>
2020-02-05 22:08:14 +0000

    update on models

--- rango/models.py
+++ rango/models.py
@@ -14,7 +14,7 @@ class Category(models.Model):
 
 
 class Page(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE())
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
