761f304834a178f7517e0bed1bbd790b2ab9d870
Ross Beech <2141893b@student.gla.ac.uk>
2020-02-05 23:53:07 +0000

    Completion of chapter 5

--- populate_rango.py
+++ populate_rango.py
@@ -36,9 +36,9 @@ def populate():
         {'title': 'Flask',
          'url': 'http://flask.pocoo.org'}]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages}}
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}}
 
     # if you want to add more categories or pages add them above
     # to the respective dictionaries
@@ -46,7 +46,7 @@ def populate():
     # the code below goes through the cats dictionary, then adds each category
     # and then adds all the associated pages for that category
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -64,7 +64,16 @@ def add_page(cat, title, url, views=0):
     return p
 
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
+
+# start execution here
+
+
+if __name__ == '__main__':
+    print('Starting Rango population script...')
+    populate()
